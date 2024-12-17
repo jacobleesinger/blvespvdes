71239a4acd6299c8515117bdc90f22647e8e6321
Jacob Singer <jacobleesinger@gmail.com>
2024-12-16 20:58:19 -0600

    feat: add supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import { invalidate } from '$app/navigation';
+  import { onMount, type Snippet } from 'svelte';
+  import type { Session, SupabaseClient } from '@supabase/supabase-js';
+
+  interface Props {
+    session: Session | null;
+    supabase: SupabaseClient;
+    children: Snippet;
+  }
+
+  let { session, supabase, children }: Props = $props();
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth');
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  });
+
 </script>
 
 {@render children()}
