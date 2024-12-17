5b88351765335fd7ae7a58047f64a928f6c7be6a
Jacob Singer <jacobleesinger@gmail.com>
2024-12-16 21:09:00 -0600

    feat: add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {}
-	},
-
-	plugins: [typography, forms]
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [typography, forms, daisyui],
+  daisyui: {
+    themes: ["light", "dark"]
+  }
 } satisfies Config;
